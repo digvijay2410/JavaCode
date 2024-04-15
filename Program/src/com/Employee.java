2838a78c8cc2dccd7f8c4f79f334e73a0417ea7a
Digvijay Kadam <digvijaykadam2410@gmail.com>
2024-04-15 22:08:48 +0530

    equals and hashcode added

--- Program/src/com/Employee.java
+++ Program/src/com/Employee.java
@@ -56,6 +56,47 @@ public class Employee {
 	public String toString() {
 		return "empId=" + empId + ", name=" + name + ", salary=" + salary + ", address=" + address ;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + empId;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (empId != other.empId)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 	
