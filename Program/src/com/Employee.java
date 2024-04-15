469bb1b42899d0febfcc0d81bd8a455287765dd8
Digvijay Kadam <digvijaykadam2410@gmail.com>
2024-04-15 21:59:48 +0530

    toString added

--- Program/src/com/Employee.java
+++ Program/src/com/Employee.java
@@ -51,6 +51,11 @@ public class Employee {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", name=" + name + ", salary=" + salary + ", address=" + address + "]";
+	}
 	
 	
 	
